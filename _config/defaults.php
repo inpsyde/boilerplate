3bd02aa5f20b768847b121c23f835f581dfbdcaa
David Naber <kontakt@dnaber.de>
2016-06-11 17:48:30 +0200

    Add encoding cookie

--- _config/defaults.php
+++ _config/defaults.php
@@ -1,13 +1,4 @@
-<?php
-/**
- * Bright Nucleus Boilerplate.
- *
- * @package   BrightNucleus\Boilerplate
- * @author    Alain Schlesser <alain.schlesser@gmail.com>
- * @license   MIT
- * @link      http://www.brightnucleus.com/
- * @copyright 2016 Alain Schlesser, Bright Nucleus
- */
+<?php # -*- coding: utf-8 -*-
 
 namespace InpsydeBoilerplate;
 
