ef4d87439d60c9e9f3d400d877b50937f927bc25
David Naber <kontakt@dnaber.de>
2016-06-12 11:19:21 +0200

    Add validators for namespace, type and license

--- _src/SetupHelper.php
+++ _src/SetupHelper.php
@@ -23,4 +23,57 @@ class SetupHelper extends Scripts\SetupHelper {
 
 		return str_replace( ' ', '-', mb_strtolower( $string ) );
 	}
+
+	/**
+	 * Return a valid license abbreviation according to SPDX recommendation
+	 *
+	 * @link https://spdx.org/licenses/
+	 *
+	 * @param string $license
+	 *
+	 * @return string
+	 */
+	public static function getSPDXLicense( $license ) {
+
+		$licenses = [
+			'mit' => 'MIT',
+			'gpl' => 'GPL-2.0' //GPL-2.0+ is deprecated
+		];
+
+		$license = strtolower( $license );
+		return isset( $licenses[ $license ] )
+			? $licenses[ $license ]
+			: $licenses[ 'mit' ];
+	}
+
+	/**
+	 * Normalizes namespace
+	 *
+	 * @param string $namespace
+	 *
+	 * @return string
+	 */
+	public static function getNamespace( $namespace ) {
+
+		$namespace = trim( $namespace );
+		$namespace = rtrim( $namespace, '\\' );
+		$namespace = str_replace( '\\\\', '\\', $namespace );
+
+		return $namespace;
+	}
+
+	/**
+	 * Get namespace for composer autoloader config. (Like Name\\Space\\)
+	 * Expecting a FQN like Foo\Bar otherwise strange things will happen
+	 *
+	 * @param $namespace
+	 *
+	 * @return string
+	 */
+	public static function getAutoloadNamespace( $namespace ) {
+
+		$namespace = rtrim( $namespace, '\\' ) . '\\';
+
+		return str_replace( '\\', '\\\\', $namespace );
+	}
 }
