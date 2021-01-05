bfefe8bbe4fab08e8b9f0a2b06b4ce25a13cebf3
brianvarskonst <brianvar.skonst@gmail.com>
2021-01-05 16:50:37 +0100

    Remove assets bundling components if its not a wordpress package

--- _src/Task/SanitizeAssetFiles.php
+++ _src/Task/SanitizeAssetFiles.php
@@ -6,13 +6,16 @@ use BrightNucleus\Boilerplate\Scripts\Task;
 use Composer\Util;
 
 /**
- * Remove style.css and functions.php if the package type
- * is not a wordpress-theme
+ * Remove Asset bundling components if the package type
+ * is not a wordpress-theme or wordpress-plugin
  *
  * @package InpsydeBoilerplate\Task
  */
 class SanitizeAssetFiles extends Task\AbstractTask
 {
+    const CONFIG_KEY = 'assets';
+    const CONFIG_TYPE_KEY = 'type';
+
     const ASSET_FILES = [
         'resources',
         '.eslintrc',
@@ -22,12 +25,23 @@ class SanitizeAssetFiles extends Task\AbstractTask
         'webpack.config.js',
     ];
 
+    const WP_TYPES = [
+        'wordpress-theme',
+        'wordpress-plugin'
+    ];
+
     public function complete()
     {
-        $useAssets = $this->getConfigKey('Placeholders', 'assets')['value'];
+        $type = $this->getConfigKey('Placeholders', self::CONFIG_TYPE_KEY)['value'];
 
         $fs = new Util\Filesystem();
         $baseDir = $this->getConfigKey('BaseDir');
+
+        if (!in_array($type, self::WP_TYPES, true)) {
+            $this->removeAssetsFiles($fs, $baseDir);
+        }
+
+        $useAssets = $this->getConfigKey('Placeholders', self::CONFIG_KEY)['value'];
 
         if ($useAssets === 'yes') {
             $fs->remove("{$baseDir}/assets");
@@ -35,6 +49,16 @@ class SanitizeAssetFiles extends Task\AbstractTask
             return;
         }
 
+        // Remove Asset Files, if the package uses static assets
+        $this->removeAssetsFiles($fs, $baseDir);
+    }
+
+    /**
+     * @param Util\Filesystem $fs
+     * @param string $baseDir
+     */
+    private function removeAssetsFiles($fs, $baseDir)
+    {
         $this->io->write("Removing asset files");
 
         foreach (self::ASSET_FILES as $file) {
